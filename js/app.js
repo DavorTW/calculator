07946658d2a2868bf6bccbd416efe0ccae171590
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-25 17:46:00 +0800

    Added functions to handle all the basic operations Added an operate function to call the basic operations functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-console.log("Hello from app.js");
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    if(num2 === 0){
+        return "You can't divide by 0 you donkey";
+    }
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator){
+    if (operator === '+') {
+        add(num1, num2);        
+    }
+    if (operator === "-") {
+        substract(num1, num2);
+    }
+    if (operator === "*") {
+        multiply(num1, num2);
+    }
+    if (operator === "/") {
+        divide(num1, num2);
+    }
+}
+
+let num1;
+let num2;
+let operator;
+
